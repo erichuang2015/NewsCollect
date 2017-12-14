78521cfec4700c272914e44560d9341ed674ce06
AllenShen <932142511@qq.com>
2017-12-15 01:00:51 +0800

    详情页设计完成

--- webapp/app.py
+++ webapp/app.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
-from flask import Flask, jsonify, current_app, request, render_template
+from flask import Flask, jsonify, current_app, request, \
+                    render_template, redirect, url_for
 from flask_script import Manager
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
@@ -7,7 +8,7 @@ import os, sys
 from bs4 import BeautifulSoup as bs
 from erros import not_found
 from info import info
-from utils import jsonp
+from utils import jsonp, get_tag_list
 
 # reload(sys)
 # sys.setdefaultencoding('utf8')
@@ -15,10 +16,29 @@ from utils import jsonp
 app = Flask(__name__, template_folder='templates')
 app.debug = True
 bootstrap = Bootstrap(app)
-db_path = os.path.abspath('..') + '\data.db'
+db_path = os.path.abspath('.') + '\data.db'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + db_path
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
-manager = Manager(app)
+db.create_all()
+
+#    __  __                       _          _
+#   |  \/  |   ___    _   _    __| |   ___  | |  ___
+#   | |\/| |  / _ \  | | | |  / _` |  / _ \ | | / __|
+#   | |  | | | (_) | | |_| | | (_| | |  __/ | | \__ \
+#   |_|  |_|  \___/   \__,_|  \__,_|  \___| |_| |___/
+
+class News(db.Model):
+    __tablename__ = 'news'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String)  # 标题
+    unit = db.Column(db.String)  # 文章所属单位
+    type = db.Column(db.String)  # 文章类型
+    time = db.Column(db.Date)  # 发布时间
+    url = db.Column(db.String)  # 文章位置
+    imgs = db.Column(db.PickleType)  # 图片链接列表
+    content = db.Column(db.Text)  # html正文
+    timestamp = db.Column(db.TIMESTAMP)  # 抓取时间戳
 
 #          _      ____    ___
 #         / \    |  _ \  |_ _|
@@ -26,9 +46,6 @@ manager = Manager(app)
 #       / ___ \  |  __/   | |
 #      /_/   \_\ |_|     |___|
 
-
-# from models import News
-import models
 @app.route('/api/unit_list')
 @jsonp
 def unit_list():
@@ -54,11 +71,14 @@ def unit_list():
     return jsonify(unit_info)
 
 
-@app.route('/api/<unit_code>')
+@app.route('/api/news/<unit_code>')
 @jsonp
 def get_news_list(unit_code):
     """
-    获取单位新闻列表
+    获取单位新闻列表 
+    params:
+        tag: 新闻tag
+        num: 获取条数
     :return:
     [ ...
     {
@@ -72,10 +92,9 @@ def get_news_list(unit_code):
     news_list = []
     tag = request.args.get('tag', None)
     num = int(request.args.get('num', 10))
-    q = models.News.query.filter_by(unit=unicode(
-        info[unit_code]['unit'])).order_by('time')
+    q = News.query.filter_by(unit=info[unit_code]['unit']).order_by('time')
     if tag is not None:
-        q = q.filter_by(type=unicode(tag))
+        q = q.filter_by(type=tag)
     q = q.all()
     if q is None:
         return not_found("items not found")
@@ -101,7 +120,7 @@ def get_news(id):
     :param id:
     :return:
     """
-    q = models.News.query.filter_by(id=id).first()
+    q = News.query.filter_by(id=id).first()
     if q is None:
         return not_found('news not found')
     soup = bs(q.content, 'html.parser')
@@ -126,14 +145,49 @@ def get_news(id):
 #          \_/    |_|  \___|   \_/\_/   |___/
 
 @app.route('/detail/<unit>')
-def show_detail(unit):
-    current_app.logger.debug(current_app.template_folder)
-    return render_template('test.html')
+def detail_redirect(unit):
+    tag_list = get_tag_list(info[unit]['tag_codes'])
+    return redirect(url_for('detail', unit=unit, tag=tag_list[0]))     
 
-
-@app.route('/')
-def index():
-    return 'test'
+@app.route('/detail/<unit>/<tag>')
+def detail(unit, tag):
+    """
+    详情页
+    params:
+        page: 页数
+    """
+    tag_list = get_tag_list(info[unit]['tag_codes'])
+    # 分页数据获取需要优化
+    q = News.query.filter_by(unit=info[unit]['unit'], type=tag).order_by('time').all()
+    max_page = len(q)
+    page = int(request.args.get('page', 1))
+    if page < 1:
+        page = 1
+    news_list = []
+    for i in range(20 * (page - 1), 20 * page)[::-1]:
+        try:
+            item = {
+                'title': q[i].title,
+                'id': q[i].id,
+                'time': q[i].time.strftime('%y-%m-%d'),
+                'url': q[i].url,
+            }
+            news_list.append(item)
+        except IndexError:
+            break
+    data = {
+        'unit_code': unit,
+        'unit': info[unit]['unit'],
+        'tag_list': tag_list,
+        'current_tag': tag,
+        'page': page,
+        'max_page': max_page,
+        'news_list': news_list,
+    }
+    print(max_page)
+    # current_app.logger.debug(url_for('detail', unit=unit, tag=tag_list[2], param=111))
+    return render_template('detail.html', data=data)  
 
 if __name__ == '__main__':
     app.run()
+ 
--- webapp/utils.py
+++ webapp/utils.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 from functools import wraps
 from flask import request, current_app
-
+from info import info
 
 def jsonp(func):
     """Wraps JSONified output for JSONP requests."""
@@ -16,3 +16,22 @@ def jsonp(func):
         else:
             return func(*args, **kwargs)
     return decorated_function
+
+
+def get_tag_list(tag_codes):
+    """递归获取tag_codes中的tag"""
+    tag_list = []
+    travel(tag_codes, tag_list)
+    return tag_list
+
+def travel(tree, node_list):
+    """递归获取树的所有叶子节点list"""
+    for node in tree:
+        if type(tree[node]) is not dict:
+            node_list.append(tree[node])
+        else:
+            travel(tree[node], node_list)
+
+
+if __name__ == '__main__':
+    print(get_tag_list(info['jxgc']['tag_codes']))
