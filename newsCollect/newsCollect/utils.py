902bcbfde3d1205597b88735be2eea6ec8702497
AllenShen <932142511@qq.com>
2017-12-27 21:51:50 +0800

    using mysql

--- newsCollect/newsCollect/utils.py
+++ newsCollect/newsCollect/utils.py
@@ -1,21 +1,25 @@
 # -*- coding: utf-8 -*-
 import json
-# import redis
+import redis
+import os
 
-def get_conf_from_json(path):
+defult_conf_path = os.path.split(os.path.abspath(__file__))[0] + '/conf.json'
+
+def get_conf_from_json(path=defult_conf_path):
     try:
-        with open(path) as f:
+        with open(path, 'r') as f:
             config = json.load(f)
-    except IOError:
+    except IOError as e:
+        print(e)
         return None
     return config
-'''
+
 def clear_redis(conf_path='conf.json'):
     conf = get_conf_from_json(conf_path)
     redis_conf = conf['redis']
     redis_db = redis.Redis(host=redis_conf['host'], port=redis_conf['port'])
     redis_db.flushdb()
-'''
+
         
 if __name__ == '__main__':
-    clear_redis()
+    print(get_conf_from_json(os.path.split(os.path.abspath(__file__))[0] + '/conf.json'))
