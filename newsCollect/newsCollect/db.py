6ad23d27b1c56f8110c8430e607cc6818c47d4f7
AllenShen <932142511@qq.com>
2018-01-01 22:23:40 +0800

    依赖本地化

--- newsCollect/newsCollect/db.py
+++ newsCollect/newsCollect/db.py
@@ -3,7 +3,7 @@ from sqlalchemy import Column, Integer, String, Date, Text, PickleType, create_e
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
-from utils import get_conf_from_json
+from .utils import get_conf_from_json
 
 conf = get_conf_from_json('conf.json')
 if conf is None:
@@ -31,23 +31,23 @@ class News(Base):
     __tablename__ = 'news'
 
     id = Column(Integer, primary_key=True)
-    title = Column(String)  # 标题
-    unit = Column(String)  # 文章所属单位
-    type = Column(String)  # 文章类型
+    title = Column(String(255))  # 标题
+    unit = Column(String(255))  # 文章所属单位
+    type = Column(String(255))  # 文章类型
     time = Column(Date)  # 发布时间
-    url = Column(String)  # 文章位置
+    url = Column(String(255))  # 文章位置
     imgs = Column(PickleType)  # 图片链接列表
     content = Column(Text)  # html正文
     timestamp = Column(TIMESTAMP)  # 抓取时间戳
 
 
 def get_session(db_name='dev.db'):
-    if using_db is 'mysql':
-        db_path = 'mysql://{}:{}@{}'.format(
-            db_conf['db_info']['mysql']['usr'], pw, db_conf['db_info']['mysql']['host'])
-    if using_db is 'sqlite':
-        db_path = 'sqlite:///' + os.path.abspath('..') + '\%s' % db_name
-    print(db_path)
+    # if using_db is 'mysql':
+        # db_path = 'mysql://{}:{}@{}'.format(
+        #    db_conf['db_info']['mysql']['usr'], pw, db_conf['db_info']['mysql']['host'])
+    # if using_db is 'sqlite':
+    #    db_path = 'sqlite:///' + os.path.abspath('..') + '\%s' % db_name
+    db_path = 'mysql+pymysql://root:ssh0912@localhost/NewCollect'
     engine = create_engine(db_path)
     Base.metadata.create_all(engine)
     db_session = sessionmaker(bind=engine)
