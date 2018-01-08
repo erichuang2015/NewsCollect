b923890eb0c43f247671abefa606b84ff22c1a4a
AllenShen <932142511@qq.com>
2018-01-08 13:58:27 +0800

    运行前删除缓存

--- newsCollect/cron.sh
+++ newsCollect/cron.sh
@@ -4,6 +4,7 @@ export PATH=$PATH:/usr/local/bin
 # 爬虫项目目录 新的部署需要更改路径
 cd /root/projects/NewsCollect/newsCollect/
 source ../env/bin/activate
+rm -rf .scrapy
 nohup python3 schedule.py > schedule.log 2>&1 &
 
 # crontab部署命令
